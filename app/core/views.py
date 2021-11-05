6ebef1ad5d27900d8fa8cbfd6a2803f3bed1c937
jerwinwieser <j2.wieser@gmail.com>
2021-11-05 10:02:23 +0100

    django setup

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+class IndexView(TemplateView):
+	template_name = 'core/index.html'
